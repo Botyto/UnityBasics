eb7d2f5043b263d3df7b846a4df86daa739ebaee
Botyto <bisserkr@gmail.com>
2022-04-04 08:40:53 +0300

    Add sample scene and test component

---
+++ Assets/TestComponent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TestComponent : MonoBehaviour
+{
+    public Modifiable ModifiableValue;
+
+    public void Start()
+    {
+        var taggedText = new TaggedText("<objname('TestObject').Transform.position>", this);
+        print($"'{taggedText.Text}' -> '{taggedText.Process()}'");
+    }
+
+    public string TestMethod(string str)
+    {
+        print(str);
+        return str;
+    }
+}
